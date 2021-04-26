d08d15ec83753dec3bf72e24fa910659a1ada3e9
Edwin <edwinclement08@gmail.com>
2021-04-26 17:38:07 +0530

    Added back Up Arrow as alternative to Space

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -26,7 +26,7 @@ func _physics_process(delta):
 
 		
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_select"):
+		if Input.is_action_just_pressed("ui_select") or Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.2)
